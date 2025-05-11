a91ef366c73c9f230d830261c4045fd8ed01978a
dadi1 <narayan.shimanoe@gmail.com>
2025-05-11 09:27:01 -0300

    começo da declaração do counter.vhd

---
+++ rtl/counter.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+library IEEE.std_logic_1164.all;
+library IEEE.numeric_std.all;
+
+--- Declaração da da entidade counter ---
+
+entity counter is
+    generic (
+        WIDTH: natural := 8 -- Tamanem em bits.
+    );
+
+    port (
+        clock, reset  : in std_logic; -- Clock e reset sincrono.
+        enable        : in std_logic; -- Habilita a contagem.
+        load          : in std_logic; -- Carga paralela.
+        up            : in std_loigc;
+        -- Tipo de contagem:
+        -- 0 : contagem descresente.
+        -- 1 : contagem crescente.
+        data_i        : in std_logic_vector(WIDTH-1 downto 0); -- Entrade paralela.
+        data_o        : in std_logic_vector(WIDTH-1 downto 0) -- Saida paralela.
+    );
+end counter; -- Fim da declaração da entidade.
