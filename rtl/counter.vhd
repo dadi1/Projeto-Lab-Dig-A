24dc4a76a4cc22b01a0d1f20e3161d990c096cb1
dadi1 <narayan.shimanoe@gmail.com>
2025-05-13 04:00:41 -0300

    implementação do counter terminada

--- rtl/counter.vhd
+++ rtl/counter.vhd
@@ -29,3 +29,33 @@ entity counter is
         data_o        : in std_logic_vector(WIDTH-1 downto 0) -- Saida paralela.
     );
 end counter; -- Fim da declaração da entidade.
+
+-- implementação da arquitetura.
+architecture comportamental of counter is
+    signal count : unsigned(WIDTH-1 downto 0);
+begin
+
+    process(clock) 
+    begin
+        if rising_edge(clock) then
+
+            if reset = '1' then
+                count <= (others => '0'); -- reset sincrono
+            
+            elsif enable = '1' then
+                if load = '1' then
+                    count <= unsigned(data_i); -- entrada de carga paralela
+                else
+                    if up = '1' then -- Contagem crescente.
+                        count <= count + 1;
+                    else -- Contagem decrescente.
+                        count <= count - 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    data_o <= std_logic_vector(count); -- Conversão para a saída.
+
+end architecture.
