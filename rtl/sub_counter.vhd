0042280cb67142bf43fae4344a9da63061eb95a6
dadi1 <narayan.shimanoe@gmail.com>
2025-05-24 16:30:09 -0300

    substitution counter added

---
+++ rtl/sub_counter.vhd
@@ -0,0 +1,10 @@
+--====================================--
+-- Narayan Shimanoe Lisboa - 14600141 --
+-- Hugo dos Reis - 12544308           --  
+--====================================--
+
+-- importando bibliotecas.
+
+library IEEE;
+library IEEE.std_logic_1164.all;
+library IEEE.numeric_std.all;
