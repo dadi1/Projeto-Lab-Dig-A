f033cc9e3c3db9ce20a4b7b726967eb411a9dba4
dadi1 <narayan.shimanoe@gmail.com>
2025-05-25 15:32:45 -0300

    substitution counter implemented in toplevel

--- rtl/project_toplevel.vhd
+++ rtl/project_toplevel.vhd
@@ -16,7 +16,15 @@ entity exp06 is
         --rx_data_out  : out std_logic_vector(7 downto 0); --saida paralela do valor recebido
         lsr_pe       : out std_logic;
         lsr_dr       : out std_logic;
-        segment_out  : out std_logic_vector(6 downto 0)
+        segment_out  : out std_logic_vector(6 downto 0);
+
+        -- saidas do contador.
+        sub_1        : out std_logic;
+        sub_2        : out std_logic;
+        sub_3        : out std_logic;
+        sub_4        : out std_logic;
+        sub_5        : out std_logic;
+        erro         : out std_logic
     );
 end exp06;
 
@@ -58,9 +66,24 @@ architecture behaviour of exp06 is
         );
     end component;
 
+    component sub_counter
+        port (
+            clock         : in  std_logic;
+            reset         : in  std_logic;
+            substitution  : in  std_logic;
+            subs_1        : out std_logic;
+            subs_2        : out std_logic;
+            subs_3        : out std_logic;
+            subs_4        : out std_logic;
+            subs_5        : out std_logic;
+            subs_erro     : out std_logic
+        );
+    end component;
+
     signal clk_1p8432MHz : std_logic;
     signal baud_clk      : std_logic;
     signal rx_data       : std_logic_vector(7 downto 0);
+    signal lsr_dr_int    : std_logic;
     
     constant BAUD_DIVISOR : std_logic_vector(15 downto 0) := x"000C";
 begin
@@ -88,8 +111,24 @@ begin
             lcr_config => lcr_config,
             rx_data    => rx_data,
             lsr_pe     => lsr_pe,
-            lsr_dr     => lsr_dr
+            lsr_dr     => lsr_dr_int
         );
+
+    lsr_dr <= lsr_dr_int;
+
+    sub_ctr: sub_counter
+        port map(
+            clock        => baud_clk,      -- ou baud_clk, conforme desejar
+            reset        => global_reset,
+            substitution => lsr_dr_int,     -- conta cada byte pronto
+            subs_1        => sub_1,
+            subs_2        => sub_2,
+            subs_3        => sub_3,
+            subs_4        => sub_4,
+            subs_5        => sub_5,
+            subs_erro     => erro
+        );
+
 
     display_conv: ascii
         port map(
--- rtl/sub_counter.vhd
+++ rtl/sub_counter.vhd
@@ -6,28 +6,28 @@
 -- importando bibliotecas.
 
 library IEEE;
-library IEEE.std_logic_1164.all;
-library IEEE.numeric_std.all;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
 --- ================================== =---
 --- Declaração da da entidade counter ---
 entity sub_counter is
     port (
-        clock, reset : in std_logic; -- Clock e reset assíncrono.
+        clock, reset  : in std_logic; -- Clock e reset assíncrono.
 
-        substitution : in std_logic;
+        substitution  : in std_logic;
         -- 0 : enquanto substituição não for feito.
         -- 1 : quando uma substituição for feita.
 
         -- sinais de saida para o número de substituição feitos
-        sub_1        : out std_logic; -- primeira substituição.
-        sub_2        : out std_logic; -- segunda substituição.
-        sub_3        : out std_logic; -- terceira substituição.
-        sub_4        : out std_logic; -- quarta substituição.
-        sub_5        : out std_logic; -- quinta substituição.
+        subs_1        : out std_logic; -- primeira substituição.
+        subs_2        : out std_logic; -- segunda substituição.
+        subs_3        : out std_logic; -- terceira substituição.
+        subs_4        : out std_logic; -- quarta substituição.
+        subs_5        : out std_logic; -- quinta substituição.
 
         -- sinal de saida para ERRO
-        erro         : out std_logic; -- saida de erro.
+        subs_erro     : out std_logic -- saida de erro.
     );
 end sub_counter; -- Fim da declaração da entidade.
 -- ====================================---
@@ -38,7 +38,7 @@ architecture comportamental of sub_counter is
 
     -- implemntação do estados.
     type estado_t is (IDLE, SUB_1, SUB_2, SUB_3, SUB_4, SUB_5, ERRO);
-    signal  PE, EA :  estado_t;
+    signal EA, PE : estado_t;
 
     -- processo de clock e mudança de estado.
 begin 
@@ -48,7 +48,7 @@ begin
             EA <= IDLE;
         elsif (rising_edge(clock)) then
             EA <= PE;
-        end if
+        end if;
     end process;
     
     -- processo da Máquina de estados.
@@ -90,18 +90,18 @@ begin
     end process;
 
     -- CONTROLE DOS LED'S.
-    sub_1 <=
-        '1' when EA = SUB_1 or EA = SUB_2 or EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or ERRO else
+    subs_1 <=
+        '1' when EA = SUB_1 or EA = SUB_2 or EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or EA = ERRO else
         '0';
-    sub_2 <= '1' when EA = SUB_2 or EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or ERRO else
+    subs_2 <= '1' when EA = SUB_2 or EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or EA = ERRO else
         '0';
-    sub_3 <= '1' when EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or ERRO else
+    subs_3 <= '1' when EA = SUB_3 or EA = SUB_4 or EA = SUB_5 or EA = ERRO else
         '0';
-    sub_4 <= '1' when EA = SUB_4 or EA = SUB_5 or ERRO else
+    subs_4 <= '1' when EA = SUB_4 or EA = SUB_5 or EA =ERRO else
         '0';
-    sub_5 <= '1' when EA = SUB_5 or EA = ERRO else
+    subs_5 <= '1' when EA = SUB_5 or EA = ERRO else
         '0';
-    erro <= '1' when EA = ERRO else
+    subs_erro <= '1' when EA = ERRO else
         '0';
 
 end comportamental; -- fim do comportamental.
